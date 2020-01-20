1540b85d43c9f1ce83caa7552e0da5af4a532a2a
gabrielluiz01 <gabriel.luizvieira01@gmail.com>
2020-01-20 15:44:38 -0300

    Updates

---
+++ precache-manifest.c240939044ddd1e407916745d730e5b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9365e92295dc2483fd5e995adb6bfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a984fb619df7a2be713",
+    "url": "/static/css/main.449fb2b6.chunk.css"
+  },
+  {
+    "revision": "719879695810f37fbb3a",
+    "url": "/static/js/2.80ba4060.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.80ba4060.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a984fb619df7a2be713",
+    "url": "/static/js/main.e1ee5274.chunk.js"
+  },
+  {
+    "revision": "b6db2c0af2dca7efe72e",
+    "url": "/static/js/runtime-main.4f57bb39.js"
+  },
+  {
+    "revision": "fde8c8b534341cd1a8e571fb6d6742d4",
+    "url": "/static/media/bacurau.fde8c8b5.jpg"
+  },
+  {
+    "revision": "e3f375f50c1e400ec9fd8e4c3a806fd8",
+    "url": "/static/media/banner.e3f375f5.jpg"
+  },
+  {
+    "revision": "3b80531ca4e0c9cf127a7f0f2cea4234",
+    "url": "/static/media/claquete.3b80531c.svg"
+  },
+  {
+    "revision": "5b23893998ae3641c0f0a064af8c64c8",
+    "url": "/static/media/corra.5b238939.jpg"
+  },
+  {
+    "revision": "58c764d940821f674cf99182cb750ee0",
+    "url": "/static/media/estrela-cinza.58c764d9.svg"
+  },
+  {
+    "revision": "e4ff01b682954f353c98b46a49d8cdb7",
+    "url": "/static/media/estrela.e4ff01b6.svg"
+  }
+]);
